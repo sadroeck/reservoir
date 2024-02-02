738cd2be74d209a496b1e7d7ca18bf22a83e2509
Sam De Roeck <sadroeck@gmail.com>
2024-02-02 15:01:33 -0800

    [impl] Add ReadBuffer

--- reservoir-ffi/src/lib.rs
+++ reservoir-ffi/src/lib.rs
@@ -21,10 +21,6 @@ pub struct ReservoirImpl {
     /// A handle to the dam thread.
     /// This is used to ensure that the dam thread is joined when the ReservoirImpl is dropped.
     dam_thread: Option<std::thread::JoinHandle<()>>,
-    /// Buffer to deserialize any reads from the reservoir
-    /// The maximum size of a transaction is 1 MiB.
-    /// Note: This is to offer mmap type behavior to any readers.
-    mem_buffer: Vec<u8>,
     /// The dam log.
     /// The dam log is used to store transaction IDs and their corresponding checksums in order of commit.
     dam_log: DamLog,
@@ -51,7 +47,6 @@ impl ReservoirImpl {
         Self {
             reservoir,
             dam_thread: Some(dam_thread),
-            mem_buffer: vec![0u8; ONE_MEBIBYTE as usize],
             dam_log,
         }
     }
@@ -65,11 +60,16 @@ impl ReservoirImpl {
         Self::RUNTIME.with(|rt| rt.block_on(self.reservoir.new_transaction_fixed(size)))
     }
 
-    fn read_into_buffer_blocking(&mut self, transaction_id: TransactionId) -> ReservoirResult<()> {
+    fn read_into_buffer_blocking(
+        &mut self,
+        transaction_id: TransactionId,
+        mem_buffer: &mut Vec<u8>,
+    ) -> ReservoirResult<()> {
+        mem_buffer.clear();
         Self::RUNTIME.with(|rt| {
             rt.block_on(async {
                 let mut reader = self.reservoir.get_transaction(transaction_id).await?;
-                reader.read_to_end(&mut self.mem_buffer).await?;
+                reader.read_to_end(mem_buffer).await?;
                 Ok(())
             })
         })
@@ -79,6 +79,21 @@ impl ReservoirImpl {
 pub struct WriteHandleImpl {
     reservoir_handle: WriteHandle<FileSlice, Arc<Event>>,
     runtime_handle: Handle,
+}
+
+pub struct ReadBuffer {
+    /// Buffer to deserialize any reads from the reservoir
+    /// The maximum size of a transaction is 1 MiB.
+    /// Note: This is to offer mmap type behavior to any readers.
+    pub mem_buffer: Vec<u8>,
+}
+
+pub struct IterHandle {
+    pub iter: DamIterator<'static>,
+    /// Buffer to deserialize any reads from the reservoir
+    /// The maximum size of a transaction is 1 MiB.
+    /// Note: This is to offer mmap type behavior to any readers.
+    pub mem_buffer: Vec<u8>,
 }
 
 #[no_mangle]
@@ -144,19 +159,17 @@ pub unsafe fn reservoir_handle_write_bytes(
 #[no_mangle]
 pub unsafe fn reservoir_get_transaction(
     reservoir: *mut ReservoirImpl,
+    read_buffer: *mut ReadBuffer,
     transaction_id: u64,
     data_out: *mut *const u8,
 ) -> i32 {
     let transaction_id = TransactionId::from(transaction_id);
     let reservoir = unsafe { &mut *(reservoir) };
-    *data_out = null();
-    reservoir.mem_buffer.clear();
-
+    let read_buffer = unsafe { &mut *(read_buffer) };
     reservoir
-        .read_into_buffer_blocking(transaction_id)
+        .read_into_buffer_blocking(transaction_id, &mut read_buffer.mem_buffer)
         .map(|_| {
-            // Set the output pointer to the start of the buffer
-            *data_out = reservoir.mem_buffer.as_mut_ptr();
+            *data_out = read_buffer.mem_buffer.as_mut_ptr();
             0
         })
         .unwrap_or_else(result_into_error_no)
@@ -193,29 +206,45 @@ pub unsafe fn reservoir_get_iter(reservoir: *mut ReservoirImpl, iter_out: *mut *
 }
 
 #[no_mangle]
+pub unsafe fn reservoir_make_buffer(buffer_out: *mut *const ReadBuffer) {
+    let boxed = Box::new(ReadBuffer {
+        mem_buffer: Vec::with_capacity(ONE_MEBIBYTE as usize),
+    });
+    *buffer_out = Box::leak(boxed) as *mut ReadBuffer;
+}
+
+#[no_mangle]
+pub unsafe fn reservoir_buffer_free(buffer: *mut ReadBuffer) {
+    if !buffer.is_null() {
+        let _ = Box::from_raw(buffer);
+    }
+}
+
+#[no_mangle]
 pub unsafe fn reservoir_iter_next(
     reservoir_impl: *mut ReservoirImpl,
-    iter: *mut DamIterator,
+    iter: *mut IterHandle,
     tx_id: *mut u64,
     size: &mut usize,
     lsn: *mut u64,
     data_out: *mut *const u8,
     is_done: *mut bool,
 ) -> i32 {
-    let iter = &mut *(iter);
+    let iter_handle = &mut *(iter);
     let reservoir = &mut *(reservoir_impl);
-    reservoir.mem_buffer.clear();
-    match iter.next() {
+
+    *data_out = null();
+    match iter_handle.iter.next() {
         Some((serialized_txn, file_offset)) => {
             *tx_id = serialized_txn.id.into();
             *size = serialized_txn.size as usize;
             *lsn = file_offset.into();
             *is_done = false;
             reservoir
-                .read_into_buffer_blocking(serialized_txn.id)
+                .read_into_buffer_blocking(serialized_txn.id, &mut iter_handle.mem_buffer)
                 .map(|_| {
                     // Set the output pointer to the start of the buffer
-                    *data_out = reservoir.mem_buffer.as_mut_ptr();
+                    *data_out = iter_handle.mem_buffer.as_mut_ptr();
                     0
                 })
                 .unwrap_or_else(result_into_error_no)
